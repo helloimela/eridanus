11625e232debb129b7cc2ca3013911f70bf67b81
Ela Yudhanira <elayudhanira@Elas-MacBook-Air.local>
2017-04-30 00:47:45 +0200

    Update content and layout

--- dist/js/main/all.js
+++ dist/js/main/all.js
@@ -1,12 +1,13 @@
 var app = angular.module('eridanus', [
-  'ngRoute'
+  'ngRoute', 'ngAnimate'
 ]);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $routeProvider
     // Home
-    .when("/", {templateUrl: "partials/home.html", controller: "HomeCtrl"})
-    .when("/portfolio",{templateUrl: "partials/portfolio.html", controller: "PortfolioCtrl"});
+    .when("/", {templateUrl: "partials/home.html", controller: "HomeCtrl", animation:"home"})
+    .when("/about", {templateUrl: "partials/about.html", controller: "AboutCtrl", animation:"about"})
+    .when("/portfolio/:itemId", {templateUrl: "partials/portfolio.html", controller: "PortItemCtrl", animation:"pt"});
     // Pages
 
     $locationProvider.html5Mode({
@@ -15,12 +16,84 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
     });
 }]);
 
-app.controller('MainCtrl', ['$http','$scope','$location',function($http, $scope, $location){
+app.controller('MainCtrl', ['$http','$scope','$location','$rootScope',function($http, $scope, $location, $rootScope){
 	$scope.go = function ( path ) {
 	  $location.path( path );
 	};
+  $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute){
+    $rootScope.animation = currRoute.animation;
+  });
+
 }]);
 
 app.controller('HomeCtrl', ['$http','$scope',function($http, $scope, $rootScope){
-	
+	$http.get('/dist/js/portfolioData.json').then(function(data){
+      $scope.items = data.data;
+      // $scope.bindHTML = $sce.trustAsHtml($scope.items);
+  });
+  $scope.pageClass = 'home-anim';
+
+}]);
+
+app.controller('AboutCtrl', ['$http','$scope',function($http, $scope, $rootScope){
+  $scope.pageClass = 'about-anim';
+
+}]);
+
+app.controller('PortItemCtrl', ['$scope','$http','$routeParams',function ($scope, $http, $routeParams ) {
+  $scope.name = 'PortItemCtrl';
+    $http.get('/dist/js/portfolioData.json').then(function(data){
+        $scope.itemDetail = data.data[$routeParams.itemId];
+    });
+    $scope.$back = function() { 
+      window.history.back();
+    };
+    $scope.pageClass='pt-anim-ctr';
+}]);
+
+app.directive('itemPortfolio', ['$location', function($location){
+  return{
+    restrict : 'E',
+    scope : {
+      item : '='
+    },
+    transclude: true,
+    templateUrl:'partials/portfolioItem.html',
+    link : function(scope, element, attrs){
+      element.on('click', function() {
+            scope.$apply(function() {
+                $location.path('/portfolio/'+scope.item.id);
+            });
+      });
+    }
+  };
+}]);
+
+app.directive('pageBg', [function(){
+  return{
+    templateUrl:'partials/pageBg.html'
+  };
+}]);
+
+app.directive('nav', ['$location',function($location){
+  return{
+    restrict : 'E',
+    templateUrl:'partials/nav.html',
+    link:function(scope,element){
+      element.on('click', function() {
+        scope.currPath = $location.$$path;
+        if(scope.currPath==='/about'){
+          scope.$apply(function() {
+            window.history.back();
+          });
+          scope.menuState='';
+        } else{
+          scope.$apply(function() {
+            $location.path('/about/');
+          });
+          scope.menuState = 'close';
+        } 
+      });
+    }
+  };
 }]);
